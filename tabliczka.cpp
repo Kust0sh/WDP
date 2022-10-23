151809f910824bd780fe0bec1a8b943b9a6914c5
Kust0sh <115213341+Kust0sh@users.noreply.github.com>
2022-10-23 19:12:36 +0200

    Add files via upload

---
+++ tabliczka.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "ile wynosi wynik dzialania " << i << "*" << i << endl;
+
+        int wynik = i * i;
+        cin >> wynik;
+   
+        if (wynik == i*i)
+    
+            cout << "poprawna odpowiedz" << endl;
+        
+        else 
+            cout << "zla odpowiedz, poprawna odpowiedz to " << i*i << endl;
+            
+    }
+    return 0;
+}
